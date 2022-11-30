25073179cdfaf6460723bc0a5538493334f05a2c
kdobro92 <kdobro92@gmail.com>
2022-11-30 22:14:41 +0900

    day19

--- udemy-typescript/basics-05-object-types/app.ts
+++ udemy-typescript/basics-05-object-types/app.ts
@@ -3,5 +3,14 @@ const person = {
   age: 30,
 };
 
+// const person = {
+//   name: "Maximilian",
+//   age: 30,
+// };
+
+// const person = {
+//   name: "Maximilian",
+//   age: 30,
+// };
 
 console.log(person.name);
